e00b0bd876a2da35850e18f1d2a11b965ceb602f
Milihhard <milihhard1996@gmail.com>
2020-06-09 21:22:17 +1000

    change last update to local datetime

--- src/main/java/org/infobourg/betterrss/models/RssFlux.kt
+++ src/main/java/org/infobourg/betterrss/models/RssFlux.kt
@@ -2,17 +2,17 @@ package org.infobourg.betterrss.models
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import java.time.OffsetDateTime
+import java.time.LocalDateTime
 
 //@Document
 @Document(collection = "RssFlux")
 data class RssFlux(
         @Id
-        var id: String,
+        var id: String="",
         var idWorkspace: String="",
         var idChannel: String="",
         var idThread: String="",
         var link: String="",
-        var lastUpdate: OffsetDateTime= OffsetDateTime.now()
+        var lastUpdate: LocalDateTime= LocalDateTime.now()
 ) {
 }

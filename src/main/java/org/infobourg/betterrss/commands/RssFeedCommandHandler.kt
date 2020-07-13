8de04a20ee2275c22cc403b7f4cfef699dae7b73
Milihhard <milihhard1996@gmail.com>
2020-07-13 17:24:03 +1000

    switch command handler to properties

--- src/main/java/org/infobourg/betterrss/commands/RssFeedCommandHandler.kt
+++ src/main/java/org/infobourg/betterrss/commands/RssFeedCommandHandler.kt
@@ -10,24 +10,22 @@ import org.bson.types.ObjectId
 import org.infobourg.betterrss.services.RssFeedService
 import org.springframework.stereotype.Component
 
+typealias  Handle = (args: List<String>, request: SlashCommandRequest) -> (SlashCommandContext) -> Response
+
 @Component
 class RssFeedCommandHandler(private val rssFeedService: RssFeedService) : CommandHandler {
 
-    private val commandHandle: Array<CommandHandle> = arrayOf(
-            CommandHandle(commandType = CommandEnum.LIST, handle = this::list),
-            CommandHandle(commandType = CommandEnum.REMOVE, handle = this::remove)
-    )
 
     override fun handleCommand(request: SlashCommandRequest): (SlashCommandContext) -> Response {
         val args = request.payload?.text?.split(" ") ?: listOf()
-        if (args.isEmpty()) return this.help();
+        if (args.isEmpty()) return this.help(args, request);
         val cmd = commandHandle.find { c -> c.commandType.command == args[0] }
-        return if (cmd != null) cmd.handle(args, request) else this.help();
+        return if (cmd != null) cmd.handle(args, request) else this.help(args, request)
     }
 
-    private fun list(args: List<String>, request: SlashCommandRequest): (SlashCommandContext) -> Response {
+    private val list: Handle = list@{ _: List<String>, _: SlashCommandRequest ->
         val rssFeed = rssFeedService.findAll()
-        return { context ->
+        return@list { context: SlashCommandContext ->
             context.ack(asBlocks(
                     section { s -> s.text(plainText("List of rss flux")) },
                     section { s -> s.text(markdownText(rssFeed.joinToString("\n") { rss -> "- ${rss.id} : ${rss.link}" })) }
@@ -35,10 +33,10 @@ class RssFeedCommandHandler(private val rssFeedService: RssFeedService) : Comman
         }
     }
 
-    private fun remove(args: List<String>, request: SlashCommandRequest): (SlashCommandContext) -> Response {
-        if(args.size < 2) {
+    private val remove: Handle = remove@{ args: List<String>, _: SlashCommandRequest ->
+        if (args.size < 2) {
             val rssFeed = rssFeedService.findAll()
-            return { context ->
+            return@remove { context ->
                 context.ack(asBlocks(
                         section { section ->
                             section
@@ -61,25 +59,30 @@ class RssFeedCommandHandler(private val rssFeedService: RssFeedService) : Comman
             }
         } else {
             val arg = args[1];
-            if(ObjectId.isValid(arg)) {
+            if (ObjectId.isValid(arg)) {
                 println("$arg is valid")
                 // If is an id
-                if(!rssFeedService.existsById(arg))
-                    return  {ctx -> ctx.ack("No Rss Feed with this id")}
+                if (!rssFeedService.existsById(arg))
+                    return@remove { ctx -> ctx.ack("No Rss Feed with this id") }
                 rssFeedService.removeByIdRssFeed((arg))
             } else {
                 // Looking or url
-                if(!rssFeedService.existsByLink(arg))
-                    return  {ctx -> ctx.ack("No Rss Feed with this link")}
+                if (!rssFeedService.existsByLink(arg))
+                    return@remove { ctx -> ctx.ack("No Rss Feed with this link") }
                 rssFeedService.removeByLink(arg)
             }
-            return { context -> context.ack("Rss Feed removed")}
+            return@remove { context -> context.ack("Rss Feed removed") }
         }
     }
 
-    private fun help(): (SlashCommandContext) -> Response {
-        return { context -> context.ack("Error in the command") }
+    private val help: Handle = help@{ _: List<String>, _: SlashCommandRequest ->
+        return@help { context -> context.ack("Error in the command") }
     }
+
+    private val commandHandle: Array<CommandHandle> = arrayOf(
+            CommandHandle(commandType = CommandEnum.LIST, handle = this.list),
+            CommandHandle(commandType = CommandEnum.REMOVE, handle = this.remove)
+    )
 }
 
 data class CommandHandle(
